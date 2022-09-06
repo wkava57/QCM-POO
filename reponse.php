c54002fc929740103d85287a4bc54b6ab956385e
kava <wabutrune.p@hotmail.com>
2022-09-06 14:19:36 +0200

    constructeur reponse

---
+++ reponse.php
@@ -0,0 +1,16 @@
+<?php
+
+class Reponse{
+
+    const BONNE_REPONSE = true;
+    const MAUVAISE_REPONSE = false;
+    public string $reponse;
+    public string $status;
+
+
+    public function __construct(string $reponse, string $status = Reponse::MAUVAISE_REPONSE)
+    {
+        $this->reponse = $reponse;
+        $this->status = $status;
+    }
+}
