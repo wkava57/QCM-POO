7d3522df45d4bdf0caa83f169a86bf68a391f652
kava <wabutrune.p@hotmail.com>
2022-09-07 13:31:01 +0200

    appreciation

--- index.php
+++ index.php
@@ -15,6 +15,14 @@ $question1->ajouterReponse(new Reponse('Des actimels'));
 $question1->setExplications('Et oui, la célèbre citation est « Et paf, ça fait des chocapics ! »');
 $qcm->ajouterQuestion($question1);
 
+$question2 = new Question('POO signifie');
+$question2->ajouterReponse(new Reponse('Php Orienté Objet'));
+$question2->ajouterReponse(new Reponse('ProgrammatiOn Orientée'));
+$question2->ajouterReponse(new Reponse('Programmation Orientée Objet', Reponse::BONNE_REPONSE));
+$question2->setExplications('Sans commentaires si vous avez eu faux :-°');
+$qcm->ajouterQuestion($question2);
+ 
+
 $qcm->setAppreciation(array('0-10' => 'Pas top du tout ...',
                             '10-20' => 'Très bien ...'));
 $qcm->generer();
--- qcm.php
+++ qcm.php
@@ -11,18 +11,51 @@ class Qcm {
     public function getQuestions(){
         return $this->questions;
     }
-    public function setAppreciation(array $appreciations): void{
-        $this->appreciations = $appreciations;
+    public function setAppreciation(array $appreciation) : Qcm
+    {
+        foreach ($appreciation as $key => $appr) {
+            if (is_numeric($key))
+                $this->appreciation[(int)$appr] = $appr;
+            else {
+                list($min, $max) = explode('-', $key);
+                if ($min > $max)
+                    list($min, $max) = array($max, $min);
+                for ($i = (int)$min; $i <= $max; $i++)
+                    $this->appreciation[$i] = $appr;
+            }
+        }
+        return $this;
     }
 
     public function generer(){
+        // si le _POST existe et s'il est rempli
         if(isset($_POST) && !empty($_POST)){
+            $note = 0;
+            foreach ($this->questions as $i => $question) {
+                echo '<br><br>';
+                echo $question -> getQuestion() . "<br><br>"; 
+                
+                if($question -> getNumBonneReponse() == $_POST[$i]){
+                    echo 'super';
+                    // chaque fois qu'il y a une bonne réponse je rajoute un à note
+                    $note++;
+                    echo $question->getExplications();
 
+                    } else {
+                        echo 'nul';
+                        echo $question->getExplications();
+                    }
+                    
+            }
+            echo "<br>";
+             echo $note . "/20";  
         }
         else{
             echo "<form method= 'post'>";
             foreach ($this->questions as $i => $question) {
+                echo '<br><br>';
             echo $question -> getQuestion() . "<br><br>";
+                
             
                 foreach ($question->getReponses() as $j => $reponse) {
                     echo "<input type = 'radio' name = '" . $i . "' value = '" . $j . "'><label for = '" . $j . "'>".$reponse->getReponse()."</label>";
