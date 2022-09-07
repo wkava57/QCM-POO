8fe332b3e8b5052077071421144b52bd7ea0a871
kava <wabutrune.p@hotmail.com>
2022-09-07 09:37:33 +0200

    QCM suite

--- qcm.php
+++ qcm.php
@@ -1,5 +1,17 @@
 <?php
 
 class Qcm {
-    
+    private array $questions;
+    private array $appreciations;
+
+    public function ajouterQuestion(Object $question){
+        array_push($this->questions, $question);
+    }
+    public function getQuestions(){
+        return $this->questions;
+    }
+    public function setAppreciation(array $appreciations): void{
+        $this->appreciations = $appreciations;
+    }
+
 }
